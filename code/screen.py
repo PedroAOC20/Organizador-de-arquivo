41cf4b543551ecd4f714f64b51c6e991dd46c7eb
PedroAOC20 <pedrodeveloperpyc@gmail.com>
2024-08-22 16:43:47 -0300

    ajustado print da tela de sucesso.

--- code/screen.py
+++ code/screen.py
@@ -1,13 +1,14 @@
 from kivy.app import App
 from kivy.lang import Builder
+from plyer import filechooser
+from organizador import organizarDir, msmFinal
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from plyer import filechooser
-from organizador import organizarDir, msmFinal
 
 GUI = Builder.load_file('Views/kview.kv')
+
 class OrganizadorApp(App):
     def build(self):
         return GUI
@@ -30,28 +31,28 @@ class OrganizadorApp(App):
                 self.root.ids.label_status.text = f"Erro: {e}"
 
     def exibir_popup_sucesso(self, mensagem):
-        layout = BoxLayout(
-            orientation='vertical',
-            padding=10, 
-            spacing=10
-        )
-        
+        # Criar um layout que vai conter a mensagem e o botão
+        layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
         label = Label(text=mensagem)
-        popup = Popup(
-            title='Sucesso!', 
-            title_size='14sp', 
-            content=layout, 
-            size_hint=(0.75, 0.5)
-        )
         
+        # Criar o Popup
+        popup = Popup(title='Sucesso!', content=layout, size_hint=(0.75, 0.5))
+        
+        # Criar o botão de "Retomar" e passar o popup como argumento
         retomar_btn = Button(
             text="Retomar", 
             size_hint_y=None, 
             height=50, 
-            on_press=popup.dismiss
+            on_press=lambda *args: self.retomar(popup)
         )
         
+        # Adicionar os widgets ao layout
         layout.add_widget(label)
         layout.add_widget(retomar_btn)
         
         popup.open()
+
+    def retomar(self, popup):
+        popup.dismiss()  # Fecha o popup
+        self.root.ids.input_diretorio.text = ''  # Limpa o campo de diretório
+        self.root.ids.label_status.text = 'Status:'  # Reseta o status
